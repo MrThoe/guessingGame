d6e38bb49d5e0040db7115ba7137a92c0a023f56
Allen Thoe <30870017+MrThoe@users.noreply.github.com>
2019-08-22 13:59:21 -0700

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -6,18 +6,19 @@ My Python Guessing Game Example2
 Date: 8/21/2019
 """
 
-
 import random
-
-def guess(low, high):
-      x = (high + low) / 2
-      return int(x)
 
 def greet():
       print(" Think of a number between 1-100 and \n I will try and guess it")
       print("Use the following codes to let me know how my guess is: ")
       print(" 1 = Too Low \n 2 = Too HIGH \n 3 = YOU GOT IT!")
 
+def guess(low, high):
+      x = (high + low) / 2
+      return int(x)
+     
+      
+      
 def checkNum(n):
       print("My guess is: " + str(n))
       check = int(input("Am I right? "))
