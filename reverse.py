f8fa0467926247b002fd010056cff5a7ea2bc6e5
Allen Thoe <30870017+MrThoe@users.noreply.github.com>
2019-08-22 14:02:00 -0700

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -8,22 +8,25 @@ Date: 8/21/2019
 
 import random
 
+#Start the game byt explaining how to play to the user
 def greet():
       print(" Think of a number between 1-100 and \n I will try and guess it")
       print("Use the following codes to let me know how my guess is: ")
       print(" 1 = Too Low \n 2 = Too HIGH \n 3 = YOU GOT IT!")
 
+#Systematically guess by taking the middle of the low and high bound. 
 def guess(low, high):
       x = (high + low) / 2
       return int(x)
      
       
-      
+#Guess the number and return the value of 1, 2, or 3 to ask the user if they are close      
 def checkNum(n):
       print("My guess is: " + str(n))
       check = int(input("Am I right? "))
       return check
 
+#Main game loop
 def play():
 
       greet()
